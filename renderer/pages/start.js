15decb498eccba92e79762ae9d4516fc3dfefd8f
Zill Ding <zilldzy@gmail.com>
2019-02-16 22:01:33 +0800

    Add prettier tooling and format files

--- renderer/pages/start.js
+++ renderer/pages/start.js
@@ -1,26 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class App extends Component {
   state = {
-    value: '3000,3001'
-  }
+    value: "3000,3001"
+  };
 
   onChange = e => {
-    this.setState({ value: e.target.value })
-  }
+    this.setState({ value: e.target.value });
+  };
 
   render() {
-    const { value } = this.state
+    const { value } = this.state;
 
     return (
       <div>
         <input value={value} onChange={this.onChange} />
         <button>refresh</button>
-        <div>3000 <button>kill</button></div>
-        <div>3001 <button>kill</button></div>
+        <div>
+          3000 <button>kill</button>
+        </div>
+        <div>
+          3001 <button>kill</button>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
