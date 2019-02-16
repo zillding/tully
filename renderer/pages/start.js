63dab4403b1bef05396c23637ffac023bd6c2f60
Zill Ding <zilldzy@gmail.com>
2019-02-16 21:52:41 +0800

    Mock home page

--- renderer/pages/start.js
+++ renderer/pages/start.js
@@ -1,3 +1,26 @@
-export default () => (
-  <div>This is Next.js speaking</div>
-)
+import React, { Component } from 'react'
+
+class App extends Component {
+  state = {
+    value: '3000,3001'
+  }
+
+  onChange = e => {
+    this.setState({ value: e.target.value })
+  }
+
+  render() {
+    const { value } = this.state
+
+    return (
+      <div>
+        <input value={value} onChange={this.onChange} />
+        <button>refresh</button>
+        <div>3000 <button>kill</button></div>
+        <div>3001 <button>kill</button></div>
+      </div>
+    )
+  }
+}
+
+export default App
