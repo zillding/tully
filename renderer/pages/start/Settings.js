012524178f5aed258def3f00c6dc9f6e61a6a52b
Zill Ding <zilldzy@gmail.com>
2019-02-23 16:21:11 +0800

    Update settings page
    - use autosize textarea
    - add title
    - update styling

--- renderer/pages/start/Settings.js
+++ renderer/pages/start/Settings.js
@@ -1,16 +1,41 @@
-import React, { Fragment } from "react";
+import React from "react";
+import Textarea from "react-textarea-autosize";
+import css from "styled-jsx/css";
+
+const { className, styles } = css.resolve`
+  textarea {
+    width: 100%;
+    font-size: inherit;
+    resize: none;
+    box-sizing: border-box;
+    border-radius: 4px;
+  }
+`;
 
 function Settings({ value, onChangeValue }) {
   return (
-    <Fragment>
-      <textarea value={value} onChange={e => onChangeValue(e.target.value)} />
+    <div>
+      <h2>Watching ports</h2>
+      <Textarea
+        className={className}
+        autoFocus
+        maxRows={10}
+        value={value}
+        onChange={e => onChangeValue(e.target.value)}
+      />
+
+      {styles}
       <style jsx>{`
-        textarea {
-          width: 100%;
-          margin: 10px;
+        div {
+          flex: 1;
+          padding: 10px;
+        }
+        h2 {
+          text-align: center;
+          margin-top: 0;
         }
       `}</style>
-    </Fragment>
+    </div>
   );
 }
 
